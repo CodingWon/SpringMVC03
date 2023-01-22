182a60651f201cb3d434e075814de0fc382554e9
CodingWon <kingcoding2298@gmail.com>
2023-01-22 10:25:15 +0900

    service 계층 구현

---
+++ SpringMVC03/src/main/java/kr/inflearn/service/BoardService.java
@@ -0,0 +1,13 @@
+package kr.inflearn.service;
+
+import java.util.List;
+
+import kr.inflearn.model.BoardVO;
+
+public interface BoardService {
+	public List<BoardVO> getList();
+	public void register(BoardVO board);
+	public BoardVO get(int bno);
+	public int remove(int bno);
+	public int modify(BoardVO board);
+}
---
+++ SpringMVC03/src/main/java/kr/inflearn/service/BoardServiceImpl.java
@@ -0,0 +1,47 @@
+package kr.inflearn.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.inflearn.mapper.BoardMapper;
+import kr.inflearn.model.BoardVO;
+
+@Service
+public class BoardServiceImpl implements BoardService{
+
+	@Autowired
+	private BoardMapper mapper;
+	
+	@Override
+	public List<BoardVO> getList() {
+		
+		return  mapper.getList();
+	}
+
+	@Override
+	public void register(BoardVO board) {
+		mapper.insert(board);
+		
+	}
+
+	@Override
+	public BoardVO get(int bno) {
+		// TODO Auto-generated method stub
+		return mapper.read(bno);
+	}
+
+	@Override
+	public int remove(int bno) {
+		// TODO Auto-generated method stub
+		return mapper.delete(bno);
+	}
+
+	@Override
+	public int modify(BoardVO board) {
+		return mapper.update(board);
+	}
+
+	
+}
